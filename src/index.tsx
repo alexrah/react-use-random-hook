d3f90d6828ff302a559f1ae4f9c20469421cdbea
alessandro stoppato <alexrah@gmail.com>
2024-09-03 13:30:01 +0200

    src/index.tsx: WIP custom react hook useRandomNumber()

---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+
+type tRandomProviderProps = {
+  children: React.ReactNode;
+  base?: number
+}
+
+export const useRandomNumber = () => {
+  const context = React.useContext(RandomContext);
+  if (context === undefined) {
+    throw new Error('useRandom must be used within a RandomProvider');
+  }
+  return context.random;
+};
+
+export const RandomNumberProvider = ({children, base = 1}: tRandomProviderProps) => {
+  const [random, ] = React.useState<number>(Math.random()*base);
+
+  return (
+    <RandomContext.Provider value={{random}}>
+      {children}
+    </RandomContext.Provider>
+  );
+};
+
+const RandomContext = React.createContext<{
+  random: number;
+} | undefined>(undefined);
