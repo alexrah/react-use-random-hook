1aed0f6df9fdbb4f1dc868d17962434ad7433917
alessandro stoppato <alexrah@gmail.com>
2024-09-04 18:53:57 +0200

    src/index.tsx: generate a randomString besides a randomNumber, add prop to customise string length

--- src/index.tsx
+++ src/index.tsx
@@ -2,27 +2,52 @@ import * as React from 'react';
 
 type tRandomProviderProps = {
   children: React.ReactNode;
-  base?: number
+  /*  */
+  /**
+   * Optional. If provided, the random number will be generated between 0 and `base`.
+   * If not provided, the random number will be generated between 0 and 1.
+   */
+  numberBase?: number;
+  /**
+   * Optional. If provided, a random string of this length will be generated and made available
+   * through the `useRandomString` hook.
+   */
+  stringLength?: number;
 }
 
-export const useRandomNumber = () => {
+function generateRandomString(stringLength: number) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < stringLength) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
+
+export const useRandom = () => {
   const context = React.useContext(RandomContext);
   if (context === undefined) {
     throw new Error('useRandom must be used within a RandomProvider');
   }
-  return context.random;
+  return { ...context };
 };
 
-export const RandomNumberProvider = ({children, base = 1}: tRandomProviderProps) => {
-  const [random, ] = React.useState<number>( () =>{return Math.random()*base});
+export const RandomProvider = ({children, numberBase = 1, stringLength = 4}: tRandomProviderProps) => {
+  const [randomNumber, ] = React.useState<number>( () =>{return Math.random()*numberBase});
+
+  const [randomString, ] = React.useState<string>( () =>{return generateRandomString(stringLength)});
 
   return (
-    <RandomContext.Provider value={{random}}>
+    <RandomContext.Provider value={{randomNumber, randomString}}>
       {children}
     </RandomContext.Provider>
   );
 };
 
 const RandomContext = React.createContext<{
-  random: number;
+  randomNumber: number;
+  randomString: string
 } | undefined>(undefined);
